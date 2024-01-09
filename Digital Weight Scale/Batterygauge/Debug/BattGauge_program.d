bf47ba17a9ac40b19249caa51233d201739d3b9c
ZeyadNashaat <96774519+ZeyadNashaat@users.noreply.github.com>
2024-01-09 21:17:06 +0200

    Add files via upload

---
+++ Digital Weight Scale/Batterygauge/Debug/BattGauge_program.d
@@ -0,0 +1,111 @@
+# FIXED
+
+BattGauge_program.obj: ../BattGauge_program.c
+BattGauge_program.obj: C:/ti/ccs1240/ccs/ccs_base/msp430/include/msp430.h
+BattGauge_program.obj: C:/ti/ccs1240/ccs/ccs_base/msp430/include/msp430i2041.h
+BattGauge_program.obj: C:/ti/ccs1240/ccs/ccs_base/msp430/include/in430.h
+BattGauge_program.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+BattGauge_program.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+BattGauge_program.obj: D:/MSP430_LABS/Batterygauge/driverlib/driverlib.h
+BattGauge_program.obj: D:/MSP430_LABS/Batterygauge/driverlib/cs.h
+BattGauge_program.obj: D:/MSP430_LABS/Batterygauge/driverlib/inc/hw_memmap.h
+BattGauge_program.obj: C:/ti/ccs1240/ccs/ccs_base/msp430/include/msp430i2xxgeneric.h
+BattGauge_program.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h
+BattGauge_program.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+BattGauge_program.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+BattGauge_program.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h
+BattGauge_program.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h
+BattGauge_program.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+BattGauge_program.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h
+BattGauge_program.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h
+BattGauge_program.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h
+BattGauge_program.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h
+BattGauge_program.obj: C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h
+BattGauge_program.obj: D:/MSP430_LABS/Batterygauge/driverlib/eusci_a_spi.h
+BattGauge_program.obj: D:/MSP430_LABS/Batterygauge/driverlib/eusci_a_uart.h
+BattGauge_program.obj: D:/MSP430_LABS/Batterygauge/driverlib/eusci_b_i2c.h
+BattGauge_program.obj: D:/MSP430_LABS/Batterygauge/driverlib/eusci_b_spi.h
+BattGauge_program.obj: D:/MSP430_LABS/Batterygauge/driverlib/flashctl.h
+BattGauge_program.obj: D:/MSP430_LABS/Batterygauge/driverlib/gpio.h
+BattGauge_program.obj: D:/MSP430_LABS/Batterygauge/driverlib/mpy.h
+BattGauge_program.obj: D:/MSP430_LABS/Batterygauge/driverlib/pmm.h
+BattGauge_program.obj: D:/MSP430_LABS/Batterygauge/driverlib/sd24.h
+BattGauge_program.obj: D:/MSP430_LABS/Batterygauge/driverlib/sfr.h
+BattGauge_program.obj: D:/MSP430_LABS/Batterygauge/driverlib/timer_a.h
+BattGauge_program.obj: D:/MSP430_LABS/Batterygauge/driverlib/tlv.h
+BattGauge_program.obj: D:/MSP430_LABS/Batterygauge/driverlib/wdt.h
+BattGauge_program.obj: ../BattGauge_interface.h
+BattGauge_program.obj: ../BattGauge_config.h
+
+../BattGauge_program.c:
+
+C:/ti/ccs1240/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1240/ccs/ccs_base/msp430/include/msp430i2041.h:
+
+C:/ti/ccs1240/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+D:/MSP430_LABS/Batterygauge/driverlib/driverlib.h:
+
+D:/MSP430_LABS/Batterygauge/driverlib/cs.h:
+
+D:/MSP430_LABS/Batterygauge/driverlib/inc/hw_memmap.h:
+
+C:/ti/ccs1240/ccs/ccs_base/msp430/include/msp430i2xxgeneric.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1240/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h:
+
+D:/MSP430_LABS/Batterygauge/driverlib/eusci_a_spi.h:
+
+D:/MSP430_LABS/Batterygauge/driverlib/eusci_a_uart.h:
+
+D:/MSP430_LABS/Batterygauge/driverlib/eusci_b_i2c.h:
+
+D:/MSP430_LABS/Batterygauge/driverlib/eusci_b_spi.h:
+
+D:/MSP430_LABS/Batterygauge/driverlib/flashctl.h:
+
+D:/MSP430_LABS/Batterygauge/driverlib/gpio.h:
+
+D:/MSP430_LABS/Batterygauge/driverlib/mpy.h:
+
+D:/MSP430_LABS/Batterygauge/driverlib/pmm.h:
+
+D:/MSP430_LABS/Batterygauge/driverlib/sd24.h:
+
+D:/MSP430_LABS/Batterygauge/driverlib/sfr.h:
+
+D:/MSP430_LABS/Batterygauge/driverlib/timer_a.h:
+
+D:/MSP430_LABS/Batterygauge/driverlib/tlv.h:
+
+D:/MSP430_LABS/Batterygauge/driverlib/wdt.h:
+
+../BattGauge_interface.h:
+
+../BattGauge_config.h:
+
